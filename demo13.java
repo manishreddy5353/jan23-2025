c4b9605be4434b44328fc4976b01a9877b73c1c5
manishreddy5353 <bmanish20849@gmail.com>
2025-01-23 15:33:10 +0530

    first commit

---
+++ demo13.java
@@ -0,0 +1,11 @@
+package jan23;
+
+public class demo13 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Welcome");
+		System.out.println("Hyderabad");
+	}
+
+}
